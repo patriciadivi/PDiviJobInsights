3e22ccd5cae5828a02a82efa43063527c20fac10
PatriciaDivi <patriciavianad@gmail.com>
2022-10-27 03:16:15 -0300

    feat(Tests): :sparkles: Implement test for "text counter" function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
     pass
+    resTestPython = count_ocurrences("src/jobs.csv", "python")
+    resTestJS = count_ocurrences("src/jobs.csv", "javaScript")
+    resTestJest = count_ocurrences("src/jobs.csv", "jest")
+    resTestMongoDB = count_ocurrences("src/jobs.csv", "mongodb")
+    resTestReact = count_ocurrences("src/jobs.csv", "react")
+    assert resTestPython == 1639
+    assert resTestJS == 122
+    assert resTestJest == 0
+    assert resTestMongoDB == 150
+    assert resTestReact == 141
